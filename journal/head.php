272f094a4da467999acaeaf34811fa1f3ce2a17f
saikatgomes <saikatgomes@gmail.com>
2015-02-16 21:43:21 -0600

    adding multiple styles

---
+++ journal/head.php
@@ -0,0 +1,23 @@
+<?php
+	$this_title="Jae Seok Lee's Webpage";
+	if (!empty($MY_TITLE)){
+		$this_title=$MY_TITLE;
+	}
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $this_title?></title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
